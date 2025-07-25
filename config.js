55aea1ce5e2b427f63ce43c3729da9372a4c3f4d
3MR-CodeX <3mr.is.studing@gmail.com>
2025-07-25 15:44:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 window.env = {
-  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8000",
-  BACKEND_API_KEY: process.env.NAMEIT2 || "fallback-key-for-local-testing"
+    BACKEND_URL: process.env.BACKEND_URL || "https://4760050f-1753-4fa4-a6d0-1c75d54d8da4-00-ownpet0g9iuq.riker.replit.dev",
+    BACKEND_API_KEY: process.env.BACKEND_API_KEY || "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"
 };
