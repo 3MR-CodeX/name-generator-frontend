d73c16c0897edf44ec990bd5925543ee84aeda36
3MR-CodeX <3mr.is.studing@gmail.com>
2025-07-26 10:20:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Environment variables with logging and fallback
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://nameit-backend-2.vercel.app";
-const API_KEY = process.env.REACT_APP_NAMEIT2 || "gsk_mTSrnV9GV9cWGdyb3FYwEOLr3LPgSwSnuLw4Umytty6";
-console.log("URL:", BACKEND_URL, "API:", API_KEY);
+const BACKEND_URL = "https://nameit-backend-2.vercel.app";
+const API_KEY = "gsk_mTSrnV9GV9cWGdyb3FYwEOLr3LPgSwSnuLw4Umytty6";
 
 const CATEGORY_OPTIONS = [
   "App", "Book", "Brand", "Company", "Course", "Drawing", "Event", "Game",
