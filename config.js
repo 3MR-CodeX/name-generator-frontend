dca2aed377f0eb1ef3db971996eef52debaac074
3MR-CodeX <3mr.is.studing@gmail.com>
2025-07-31 09:33:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,16 +8,15 @@ window.env = {
 // REPLACE THIS WITH YOUR ACTUAL FIREBASE CONFIG OBJECT
 // You can find this in your Firebase Project Settings -> General -> Your apps
 window.__firebase_config = `{
-  apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
-  authDomain: "nameit-app-nit.firebaseapp.com",
-  projectId: "nameit-app-nit",
-  storageBucket: "nameit-app-nit.firebasestorage.app",
-  messagingSenderId: "255958152641",
-  appId: "1:255958152641:web:d56fa5b2c95945bac5ab40",
-  measurementId: "G-J9XGX5BVFR"
+  "apiKey": "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
+  "authDomain": "nameit-app-nit.firebaseapp.com",
+  "projectId": "nameit-app-nit",
+  "storageBucket": "nameit-app-nit.firebasestorage.app",
+  "messagingSenderId": "255958152641",
+  "appId": "1:255958152641:web:d56fa5b2c95945bac5ab40",
+  "measurementId": "G-J9XGX5BVFR"
 }`;
 
 // This token is provided by the Canvas environment for initial authentication.
 // If not in Canvas, it will be undefined, and anonymous sign-in will be used.
 window.__initial_auth_token = typeof __initial_auth_token !== 'undefined' ? __initial_auth_token : null;
-
