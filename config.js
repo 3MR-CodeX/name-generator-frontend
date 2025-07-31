ec1990c32281bfe77030e0f0ba0eaa5fb364c565
3MR-CodeX <3mr.is.studing@gmail.com>
2025-07-31 09:07:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,13 @@ window.env = {
 // REPLACE THIS WITH YOUR ACTUAL FIREBASE CONFIG OBJECT
 // You can find this in your Firebase Project Settings -> General -> Your apps
 window.__firebase_config = `{
-  "apiKey": "YOUR_FIREBASE_API_KEY",
-  "authDomain": "YOUR_PROJECT_ID.firebaseapp.com",
-  "projectId": "YOUR_PROJECT_ID",
-  "storageBucket": "YOUR_PROJECT_ID.appspot.com",
-  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
-  "appId": "YOUR_APP_ID"
+  apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
+  authDomain: "nameit-app-nit.firebaseapp.com",
+  projectId: "nameit-app-nit",
+  storageBucket: "nameit-app-nit.firebasestorage.app",
+  messagingSenderId: "255958152641",
+  appId: "1:255958152641:web:d56fa5b2c95945bac5ab40",
+  measurementId: "G-J9XGX5BVFR"
 }`;
 
 // This token is provided by the Canvas environment for initial authentication.
