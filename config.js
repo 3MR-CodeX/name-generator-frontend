b7ed457a8b1494dd413e0286658f5da4f05a9fe8
3MR-CodeX <3mr.is.studing@gmail.com>
2025-07-31 14:28:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-// config.js
-window.env = {
-  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8000",
-  BACKEND_API_KEY: process.env.BACKEND_API_KEY || "fallback-key-for-local-testing"
+// frontend/config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
+  authDomain: "nameit-app-nit.firebaseapp.com",
+  projectId: "nameit-app-nit",
+  storageBucket: "nameit-app-nit.firebasestorage.app",
+  messagingSenderId: "255958152641",
+  appId: "1:255958152641:web:d56fa5b2c95945bac5ab40",
+  measurementId: "G-J9XGX5BVFR"
 };
+
+export default firebaseConfig;
