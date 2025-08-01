5e97e70617e42a0a92e0f223ad605b93f15b4fd7
3MR-CodeX <3mr.is.studing@gmail.com>
2025-08-01 20:48:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,5 @@
+// config.js
 window.env = {
-  BACKEND_URL: "https://nameit-backend-2.vercel.app", // Use your deployed backend URL
-  BACKEND_API_KEY: "fallback-key-for-local-testing", // Replace with actual key if needed
-  FIREBASE_CONFIG: {
-    apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
-    authDomain: "nameit-app-nit.firebaseapp.com",
-    projectId: "nameit-app-nit",
-    storageBucket: "nameit-app-nit.firebasestorage.app",
-    messagingSenderId: "255958152641",
-    appId: "1:255958152641:web:d56fa5b2c95945bac5ab40",
-    measurementId: "G-J9XGX5BVFR"
-  }
+  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8000",
+  BACKEND_API_KEY: process.env.BACKEND_API_KEY || "fallback-key-for-local-testing"
 };
