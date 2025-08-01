cf0011227eb0d6b6e0b45e3ed248a71bb121950e
3MR-CodeX <3mr.is.studing@gmail.com>
2025-08-01 21:23:25 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+// Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
+  authDomain: "nameit-app-nit.firebaseapp.com",
+  projectId: "nameit-app-nit",
+  storageBucket: "nameit-app-nit.firebasestorage.app",
+  messagingSenderId: "255958152641",
+  appId: "1:255958152641:web:d56fa5b2c95945bac5ab40",
+};
+
+export { firebaseConfig };
