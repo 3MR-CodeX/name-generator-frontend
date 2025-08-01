4f2c8e9875e156c90ad996c63bd544a3d3dccc01
3MR-CodeX <3mr.is.studing@gmail.com>
2025-08-01 21:21:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-// config.js
-window.env = {
-  BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8000",
-  BACKEND_API_KEY: process.env.BACKEND_API_KEY || "fallback-key-for-local-testing"
-};
+
