ce7d428f01ef244133fc45bc9712ca94e2560a7c
3MR-CodeX <3mr.is.studing@gmail.com>
2025-08-02 22:04:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
-
+// config.js
+window.env = {
+  BACKEND_URL: "https://nameit-backend-2.vercel.app",
+  
+  // NEW: Add your Firebase project configuration here
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
+    authDomain: "nameit-app-nit.firebaseapp.com",
+    projectId: "nameit-app-nit",
+    storageBucket: "nameit-app-nit.appspot.com",
+    messagingSenderId: "255958152641",
+    appId: "1:255958152641:web:d56fa5b2c95945bac5ab40"
+  }
+};
