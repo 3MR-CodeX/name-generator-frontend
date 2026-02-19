9d1d7a44644624983557596d23b13cdd60c2562a
3MR-CodeX <3mr.is.studing@gmail.com>
2026-02-19 14:51:21 +0200

    Refactor config.js for new backend configuration

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
-// config.js
-window.env = {
-  BACKEND_URL: "https://nameit-backend-2.vercel.app",
-  
-  // NEW: Add your Firebase project configuration here
-  FIREBASE_CONFIG: {
-    apiKey: "AIzaSyDT7kkBgflIKI432uxY_piFueCzmqmPD6U",
-    authDomain: "nameit-app-nit.firebaseapp.com",
-    projectId: "nameit-app-nit",
-    storageBucket: "nameit-app-nit.appspot.com",
-    messagingSenderId: "255958152641",
-    appId: "1:255958152641:web:d56fa5b2c95945bac5ab40"
-  }
-};
+{
+  "functions": {
+    "main.py": {
+      "runtime": "vercel-python@0.1.0"
+    }
+  },
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "main.py"
+    }
+  ]
+}
